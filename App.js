45e2b92235ac66c579d37c430fc7583459512416
ykdevelops <khal0160@algonquinlive.com>
2021-11-30 17:37:30 -0500

    Displaying BB API data on dahboard for initial testing

--- App.js
+++ App.js
@@ -1,28 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-// import HomeScreen from './screens/HomeScreen';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import Dashboard from './screens/Dashboard'
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator()
 
-
-
-export default function App() {
+export default function App () {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name='Dashboard' component={Dashboard} />
       </Stack.Navigator>
     </NavigationContainer>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/Dashboard.js
@@ -0,0 +1,61 @@
+
+import { StatusBar } from 'expo-status-bar'
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, Text, View, SafeAreaView, FlatList  } from 'react-native'
+
+export default function Dashboard () {
+  const baseURL = 'https://www.breakingbadapi.com/api/'
+  const [characters, setCharacters] = useState([])
+
+  useEffect(() => {
+    fetch(`${baseURL}characters`)
+      .then(resp => {
+        if (!resp.ok) throw new Error(resp.statusText)
+        return resp.json()
+      })
+      .then(data => {
+        
+        let results = data.map((item, index) => {
+          return { ...item, key: index + 7 }
+        })
+        setCharacters(results)
+      })
+      .catch(console.error)
+  }, [])
+  return (
+    <SafeAreaView style={styles.container} edges={['right', 'bottom', 'left']}>
+      <FlatList
+        data={characters}
+        renderItem={(item) => 
+          <Character 
+          
+          character={item}/>
+        }
+      />
+      <StatusBar style='auto' />
+    </SafeAreaView>
+  )
+}
+function Character({character}){
+    //props.character
+    //props.character.item  props.character.index
+    return(
+      <View style={styles.row}>
+        <Text>{character.item.name}</Text>
+      </View>
+    )
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: '100%'
+    },
+    row: {
+        margin: '10%'
+    }
+  })
+
