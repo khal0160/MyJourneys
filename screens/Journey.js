10abd1ae723b0cb3817a79e6990f8d311ec4eff0
ykdevelops <khal0160@algonquinlive.com>
2021-11-30 18:53:43 -0500

    Added still google maps view on Journey page

--- screens/Journey.js
+++ screens/Journey.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
+import MapView from "react-native-maps";
 
 export default function Journey ({ navigation, route }) {
   console.log(navigation)
@@ -8,7 +9,17 @@ export default function Journey ({ navigation, route }) {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Journey Page</Text>
-      
+      <MapView
+        style={styles.map}
+        //specify our coordinates.
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+
       <StatusBar style='auto' />
     </View>
   )
@@ -17,8 +28,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    alignItems: 'center'
   },
   mediumPoster: {
     
@@ -27,5 +37,9 @@ const styles = StyleSheet.create({
   },
   title: {
     paddingVertical: 20
-  }
+  },
+  map: {
+    width: 350,
+    height: 350
+  },
 })
